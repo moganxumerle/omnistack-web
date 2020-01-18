c5ffd7c61c144303f8dcb704c407858e40cfee06
Mogan Xumerle <moganrz@gmail.com>
2020-01-18 10:43:12 -0300

    Criando form de cadastro

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+      </aside>
+      <main>
+      
+      </main>
+    </div>
   );
 }
 
